46330017407cf6dbde5fc9cf002a1571fe652ae4
IINova1 <142625192+IINova1@users.noreply.github.com>
2025-09-29 16:52:47 -0300

    semilla

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -76,6 +76,9 @@ class Nutricional(models.Model):
         managed = False
         db_table = 'Nutricional'
 
+    def __str__(self):
+        return f"ingredientes {self.id}"
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=300, blank=True, null=True)

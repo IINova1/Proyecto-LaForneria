296a3d089dcc617a829aaeef860f1e65946ddc2f
rafa <RAFAEL.ORREGO02@INACAPMAIL.CL>
2025-11-11 13:01:01 -0300

    ACTUALIZACION PROVEDORES

--- monitoreo/proveedores/urls.py
+++ monitoreo/proveedores/urls.py
@@ -1,11 +1,16 @@
+# proveedores/urls.py
 from django.urls import path
 from . import views
 
-app_name = 'proveedores'  # <--- MUY IMPORTANTE por el namespace
+app_name = 'proveedores'
 
 urlpatterns = [
+    # --- CRUD de Proveedores ---
     path('', views.listar_proveedores, name='listar_proveedores'),
     path('nuevo/', views.crear_proveedor, name='crear_proveedor'),
-    path('editar/<int:pk>/', views.editar_proveedor, name='editar_proveedor'),
-    path('eliminar/<int:pk>/', views.eliminar_proveedor, name='eliminar_proveedor'),
+    path('<int:pk>/editar/', views.editar_proveedor, name='editar_proveedor'),
+    path('<int:pk>/eliminar/', views.eliminar_proveedor, name='eliminar_proveedor'),
+
+    # --- Exportación Excel ---
+    path('exportar/', views.exportar_proveedores_excel, name='exportar_proveedores_excel'),
 ]
